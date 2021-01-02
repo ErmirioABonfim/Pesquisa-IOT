41bf0fb11131cf0e41e4bf99ebe2dc29ac5c246c
Ermirio Alves Bonfim <ermirio.contato@gmail.com>
2021-01-02 11:47:24 -0300

    Update arduino.ino
    Subistituido valor serial por valor de entrada analógica.

--- arduino/arduino/arduino.ino
+++ arduino/arduino/arduino.ino
@@ -1,12 +1,39 @@
-int counter = 0;
+/*
+  AnalogReadSerial
 
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
+*/
+#include <Servo.h>
+Servo myservo;  // create servo object to control a servo
+
+// the setup routine runs once when you press reset:
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  myservo.attach(9);
+  pinMode(13,OUTPUT);
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-
-Serial.print(random(150,300), DEC);
-delay(2000);
+  // read the input on analog pin 0:
+  int sensorValue = map(analogRead(A0),0,1023,0,180);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  myservo.write(sensorValue); 
+  if (sensorValue > 100) 
+  {
+    digitalWrite(13,HIGH);
+    
+  }
+  else{
+    digitalWrite(13,LOW);
+  }
+  delay(10);        // delay in between reads for stability
 }
